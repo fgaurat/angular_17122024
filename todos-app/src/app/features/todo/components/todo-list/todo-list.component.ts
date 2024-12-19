6f654bccbd0fd58a827f1bce98985d0b2adbbb1f
Frédéric GAURAT <fgaurat@gmail.com>
2024-12-19 15:27:56 +0100

    add delete

--- todos-app/src/app/features/todo/components/todo-list/todo-list.component.ts
+++ todos-app/src/app/features/todo/components/todo-list/todo-list.component.ts
@@ -1,14 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
-import { Todos } from '../../models/todo';
-import { EMPTY, Observable } from 'rxjs';
+import { Todo, Todos } from '../../models/todo';
+import { EMPTY, Observable, switchMap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import {MatTableModule} from '@angular/material/table';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [CommonModule,MatTableModule,MatCheckboxModule],
+  imports: [CommonModule,MatTableModule,MatCheckboxModule,FormsModule,MatButtonModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
@@ -16,10 +18,26 @@ export class TodoListComponent implements OnInit{
   todoService:TodoService = inject(TodoService)
   // todos$:Observable<Todos> = EMPTY
   todos$!:Observable<Todos>
-  displayedColumns: string[] = ['id','title','completed','chkCompleted'];
+  displayedColumns: string[] = ['id','title','completed','chkCompleted','actions'];
 
   ngOnInit(): void {
     this.todos$ = this.todoService.getAll()
+  }
+
+  delete(todo:Todo){
+    // supprime la todo localement
+    // reload les datas du serveur <=
+
+    // C'est moche !
+    // this.todoService.delete(todo).subscribe(()=>{
+    //   this.todos$ = this.todoService.getAll()
+    // })
+
+    // C'est pas moche !
+    this.todos$ =this.todoService.delete(todo).pipe(
+      switchMap((data) => this.todoService.getAll())
+    )
+
   }
 
 }
--- todos-app/src/app/features/todo/services/todo.service.ts
+++ todos-app/src/app/features/todo/services/todo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Todos } from '../models/todo';
+import { Todo, Todos } from '../models/todo';
 import { environment } from '../../../../environments/environment';
 
 @Injectable({
@@ -13,5 +13,10 @@ export class TodoService {
 
   getAll():Observable<Todos>{
       return this.http.get<Todos>(environment.url_todos)
+  }
+
+  delete(todo:Todo):Observable<void>{
+    const url = `${environment.url_todos}/${todo.id}`
+      return this.http.delete<void>(url)
   }
 }
