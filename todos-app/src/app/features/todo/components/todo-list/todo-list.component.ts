94ef01a0bf207f33cf9df8ed98709da0336e361f
Frédéric GAURAT <fgaurat@gmail.com>
2024-12-19 14:47:39 +0100

    add checkbox

--- todos-app/src/app/features/todo/components/todo-list/todo-list.component.ts
+++ todos-app/src/app/features/todo/components/todo-list/todo-list.component.ts
@@ -1,27 +1,25 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import { Todos } from '../../models/todo';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [CommonModule,MatTableModule],
+  imports: [CommonModule,MatTableModule,MatCheckboxModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent implements OnInit{
   todoService:TodoService = inject(TodoService)
-  todos$?:Observable<Todos>
-
+  // todos$:Observable<Todos> = EMPTY
+  todos$!:Observable<Todos>
+  displayedColumns: string[] = ['id','title','completed','chkCompleted'];
 
   ngOnInit(): void {
     this.todos$ = this.todoService.getAll()
   }
-
-
-
-
 
 }
